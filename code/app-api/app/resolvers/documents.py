afdec9b28ab11da92a1866e63f68c74bd977f729
ruwen <ruwen.jin@optimizely.com>
2024-08-30 13:36:25 +0200

    more property on document

--- code/app-api/app/resolvers/documents.py
+++ code/app-api/app/resolvers/documents.py
@@ -16,16 +16,39 @@ logger = logging.getLogger(__name__)
 class Document:
     id: str
     name: str
+    filename: str
+    fileid: str
+    ctime: str
+    mtime: str
+    status: str
+    jsonstring: str
+    tags : list[str]
+
+@strawberry.type
+class DocumentFile:
+    id:str
+    name:str
+    jsonstring:str
 
 def list_documents():
-    result = scriveRequest('/api/v2/documents/list')
-    return result
+    resultJson = scriveRequest('/api/v2/documents/list')
+    print(resultJson)
+    return [Document(id=doc["id"],name=doc["title"],filename=doc['file']['name'] if doc['file'] else "", fileid=doc['file']['id']  if doc['file'] else "", ctime=doc['ctime'],mtime=doc['mtime'],tags=doc['tags'],status=doc['status'], jsonstring=json.dumps(doc)) for doc in resultJson['documents']]
+
+def get_documentFile(document_id:str, file_name:str):
+    resultJson = scriveRequest('/api/v2/documents/' + document_id + '/files/main/' + file_name)
+
+    return DocumentFile(id=id, jsonstring=json.dumps(resultJson),name="s")
 
 @strawberry.type
 class Query:
     @strawberry.field
     def documents(self) -> list[Document]:
         return list_documents()
+
+    @strawberry.field
+    def documentFile(self, id:str, filename:str) -> DocumentFile:
+        return get_documentFile(id, filename)
 
 def scriveRequest(query):
     base_url = 'https://api-testbed.scrive.com'
@@ -35,8 +58,4 @@ def scriveRequest(query):
         + f'oauth_token="{os.environ["accesstoken"]}", ' \
         + f'oauth_signature="{os.environ["apisecret"]}&{os.environ["accesssecret"]}"'
     result = requests.get(base_url+query,headers={"Authorization":auth_header})
-    print(result.status_code)
-
-    resultJson = json.loads(result.text)
-    return [Document(id=doc["id"],name=doc["title"]) for doc  in resultJson['documents']]
-    
+    return json.loads(result.text)
