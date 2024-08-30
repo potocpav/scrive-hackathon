0c6716d00585bd9f7c010e10f4a5aac4de3e7aec
ruwen <ruwen.jin@optimizely.com>
2024-08-30 14:42:07 +0200

    save file to couchbase

--- code/app-api/app/resolvers/documents.py
+++ code/app-api/app/resolvers/documents.py
@@ -9,6 +9,7 @@ from ..auth import IsAuthenticated
 import requests
 import os
 import json
+import base64
 
 logger = logging.getLogger(__name__)
 
@@ -28,17 +29,53 @@ class Document:
 class DocumentFile:
     id:str
     name:str
-    jsonstring:str
+    endpoint:str
 
 def list_documents():
     resultJson = scriveRequest('/api/v2/documents/list')
     print(resultJson)
     return [Document(id=doc["id"],name=doc["title"],filename=doc['file']['name'] if doc['file'] else "", fileid=doc['file']['id']  if doc['file'] else "", ctime=doc['ctime'],mtime=doc['mtime'],tags=doc['tags'],status=doc['status'], jsonstring=json.dumps(doc)) for doc in resultJson['documents']]
 
-def get_documentFile(document_id:str, file_name:str):
-    resultJson = scriveRequest('/api/v2/documents/' + document_id + '/files/main/' + file_name)
+def streamTheFile(docPath:str):
+    try:
+        isExist = cb.get(env.get_couchbase_conf(),
+                        cb.DocRef(bucket=env.get_couchbase_bucket(),
+                                    collection='docs',
+                                    key=docPath))
+    except:
+        pass
 
-    return DocumentFile(id=id, jsonstring=json.dumps(resultJson),name="s")
+    if isExist:
+        return isExist['base64string']
+    else:
+        return None
+
+def get_documentFile(document_id:str, file_name:str):
+    result = scriveRequestFile('/api/v2/documents/' + document_id + '/files/main/' + file_name)
+
+    base64string = base64.b64encode(result.content)
+    #docFolder = "" # "tempfiles/"
+    docPath = document_id + "_" + file_name
+    # if not os.path.exists(docFolder + docPath):
+    #     with open(docFolder + docPath,"wb") as f:
+    #         f.write(result.content)
+
+    isExist = False
+    try:
+        isExist = cb.get(env.get_couchbase_conf(),
+                        cb.DocRef(bucket=env.get_couchbase_bucket(),
+                                    collection='docs',
+                                    key=docPath))
+    except:
+        pass    
+    if not isExist:
+        cb.insert(env.get_couchbase_conf(),
+                cb.DocSpec(bucket=env.get_couchbase_bucket(),
+                             collection='docs',
+                            key=docPath,
+                            data={'base64string': str(base64string)}))
+
+    return DocumentFile(id=document_id, endpoint=docPath,name=file_name)
 
 @strawberry.type
 class Query:
@@ -49,13 +86,23 @@ class Query:
     @strawberry.field
     def documentFile(self, id:str, filename:str) -> DocumentFile:
         return get_documentFile(id, filename)
-
-def scriveRequest(query):
-    base_url = 'https://api-testbed.scrive.com'
+    
+def get_auth_header():
     auth_header \
         = 'oauth_signature_method="PLAINTEXT", ' \
         + f'oauth_consumer_key="{os.environ["apitoken"]}", ' \
         + f'oauth_token="{os.environ["accesstoken"]}", ' \
         + f'oauth_signature="{os.environ["apisecret"]}&{os.environ["accesssecret"]}"'
+    return auth_header
+
+def scriveRequestFile(query):
+    base_url = 'https://api-testbed.scrive.com'
+    auth_header = get_auth_header()
+    result = requests.get(base_url+query,headers={"Authorization":auth_header})
+    return result
+
+def scriveRequest(query):
+    base_url = 'https://api-testbed.scrive.com'
+    auth_header = get_auth_header()
     result = requests.get(base_url+query,headers={"Authorization":auth_header})
     return json.loads(result.text)
