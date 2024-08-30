f319bf10acb80a5d06e49188b2a18e6e6d3303fc
Pavel Potoček <pavelpotocek@gmail.com>
2024-08-30 14:09:10 +0200

    Add document details to FE

--- code/web-app/src/components/Document.tsx
+++ code/web-app/src/components/Document.tsx
@@ -4,35 +4,61 @@ import {
   DOCUMENTS,
 } from '../graphql/documents'
 
-interface Item {
+interface DocumentT {
   id: string
   name: string
+  filename: string
+  ctime: string
+  mtime: string
+  status: string
+  jsonstring: string
+  tags: [string]
 }
 
 interface Props {
-  documentId: string,
+  document: DocumentT,
   setPage: Function,
 }
 
-const Document: React.FC<Props> = ({documentId, setPage}) => {
+const Document: React.FC<Props> = ({document, setPage}) => {
   const { data, loading, error } = useQuery(DOCUMENTS)
 
+  console.log(document);
 
   return (
     <div className="min-h-screen flex flex-col">
       <div className="navbar bg-base-300 text-neutral-content">
         <div className="flex-1">
           <h1 className="text-2xl">
-              Document {documentId}
+              {document.name}
           </h1>
-
         </div>
       </div>
       <div className="hover:underline hover:cursor-pointer" onClick={() => setPage({"name": "list"})}>
         Back to document list
+      </div>
+
+      <div>
+        ID: {document.id}
+      </div>
+      <div>
+        Created at: {document.ctime}
+      </div>
+      <div>
+        Modified at: {document.mtime}
+      </div>
+      <div>
+        Status: {document.status}
+      </div>
+      <div>
+        tags: {document.tags}
+      </div>
+      <div>
+        JSON: {document.jsonstring}
       </div>
     </div>
   )
 }
 
 export default Document
+export type {DocumentT}
